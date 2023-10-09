ba5f4c0bc793b7b416655a40c462781cc3226423
k4th4 <olschok@gmail.com>
2023-10-09 11:46:14 +0000

    add projects page with translation for logged in staff

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,11 @@
 from django import forms
-
-from .models import Post
+from .models import Post, TextEntry
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+
+class TextEntryForm(forms.Form):
+    text_to_translate = forms.CharField(widget=forms.Textarea(attrs={ 'style': 'height: 6em;'}))
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, TextEntryForm, TextEntry
+import openai
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -39,3 +42,33 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def text_entry_view(request):
+    if request.method == 'POST':
+        form = TextEntryForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text_to_translate']
+            text = translate_with_ai(text)
+    else:
+        form = TextEntryForm()
+        text = ''
+
+    return render(request, 'misc/projects.html' ,{'form': form, 'text': text})
+
+def translate_with_ai(text):
+  openai.api_key = os.getenv("OPENAI_KEY")
+  
+  
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=
+    "translate into English: \n\n\n"
+    + text,
+    temperature=0,
+    max_tokens=1338,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0)
+  completion = response.choices[0].text
+
+  return completion.strip()
