717b5bf6e71a8b95de22e2828bfa69338308d2c0
k4th4 <olschok@gmail.com>
2023-10-09 15:31:36 +0200

    deactivate openai

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm, TextEntryForm, TextEntry
-import openai
+#import openai
 #from dotenv import load_dotenv
 import os
 
@@ -56,20 +56,21 @@ def text_entry_view(request):
     return render(request, 'misc/projects.html' ,{'form': form, 'text': text})
 
 def translate_with_ai(text):
-  openai.api_key = '1234'
-  openai.api_key = os.getenv("OPENAI_KEY")
+    return ''
+  #openai.api_key = '1234'
+  #openai.api_key = os.getenv("OPENAI_KEY")
   
   
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=
-    "translate into English: \n\n\n"
-    + text,
-    temperature=0,
-    max_tokens=1338,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0)
-  completion = response.choices[0].text
+  #response = openai.Completion.create(
+  #  model="text-davinci-003",
+  #  prompt=
+  #  "translate into English: \n\n\n"
+  ##  + text,
+  #  temperature=0,
+  #  max_tokens=1338,
+  #  top_p=1,
+  #  frequency_penalty=0,
+  #  presence_penalty=0)
+ # completion = response.choices[0].text
 
-  return completion.strip()
+ # return completion.strip()
