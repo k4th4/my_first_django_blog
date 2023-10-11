ef7c59498cb6e47e2fc5392b56756db058aea60d
k4th4 <olschok@gmail.com>
2023-10-11 10:34:49 +0200

    Update views.py
    remove deepl

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm, TextEntryForm, TextEntry
-import deepl
+#import deepl
 import openai
 #from dotenv import load_dotenv
 import os
