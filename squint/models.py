5375b14df187d50832c0ae0da4a70f3864b022a5
k4th4 <olschok@gmail.com>
2023-10-12 14:35:39 +0000

    remove image path

--- squint/models.py
+++ squint/models.py
@@ -1,16 +1,20 @@
 from django.db import models
+import os
+from django.conf import settings
 
 def images_path():
-    return os.path.join(settings.LOCAL_FILE_DIR, "images")
+    return ''
+    #return os.path.join(os.path.dirname(os.path.dirname(__file__)),'media/documents/GDRAT.xls')
 
 class NotanLevelChoice(models.IntegerChoices):
     TWO = 2, 'two'
     THREE = 3, 'three'
     FOUR = 4, 'four'
 
+
 class SqImage(models.Model):
     is_grey = models.BooleanField(default=True)
-    image_path = models.FilePathField(path=images_path)
+    #image_path = models.FilePathField(path='/images/')
     notan_levels = models.IntegerField(default=NotanLevelChoice.TWO, choices=NotanLevelChoice.choices)
     posterize_level = models.IntegerField(default=3)
     blur_level = models.IntegerField(default=7)
